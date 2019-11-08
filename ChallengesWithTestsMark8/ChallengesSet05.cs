c6914c40753e1c9e9eca1b9da4e498e3b15ff8d9
mwelch1194@gmail.com <mwelch1194@gmail.com>
2019-11-08 10:09:39 -0600

    new

--- ChallengesWithTestsMark8/ChallengesSet05.cs
+++ ChallengesWithTestsMark8/ChallengesSet05.cs
@@ -9,7 +9,6 @@ namespace ChallengesWithTestsMark8
         {
             return ((startNumber / n) + 1) * n;
         }
-
         public void ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(Business[] businesses)
         {
             for (int i = 0; i < businesses.Length; i++)
@@ -20,12 +19,16 @@ namespace ChallengesWithTestsMark8
                 }
             }
         }
-
         public bool IsAscendingOrder(int[] numbers)
         {
             if (numbers == null)
+            {
                 return false;
-
+            }
+            if (numbers.Length == 0)
+            {
+                return false;
+            }
             for (int i = 1; i < numbers.Length; i++)
             {
                 if (numbers[i] < numbers[i - 1])
@@ -33,14 +36,15 @@ namespace ChallengesWithTestsMark8
                     return false;
                 }
             }
-
             return true;
         }
-
         public int SumElementsThatFollowAnEven(int[] numbers)
         {
             int sum = 0;
-
+            if (numbers == null)
+            {
+                return 0;
+            }
             for (int i = 1; i < numbers.Length; i++)
             {
                 if (numbers[i - 1] % 2 == 0)
@@ -48,62 +52,57 @@ namespace ChallengesWithTestsMark8
                     sum += numbers[i];
                 }
             }
-
             return sum;
         }
-
         public string TurnWordsIntoSentence(string[] words)
         {
-            if (words == null || words.Length == 0)
+            if (words == null || words.Length == 0 || words.Length == 1)
             {
                 return "";
             }
-
             string sentence = "";
-
-            foreach (string word in words)
+            int counter = 0;
+            foreach (string str in words)
             {
-                if (word.Trim().Length > 0)
+                if (str == " " || str == "")
                 {
-                    sentence += word.Trim() + " ";
+                    counter++;
+                    continue;
                 }
+                string toAdd = str.Replace(" ", "");
+                sentence += toAdd;
+                if (counter < words.Length - 1 && toAdd != "") sentence += " ";
+                else if (counter == words.Length - 1) sentence += ".";
+                counter++;
             }
-
-            if (sentence.Length == 0)
-            {
-                return "";
-            }
-
-            sentence = sentence.Substring(0, sentence.Length - 1);
-            sentence += ".";
             return sentence;
         }
-
         public double[] GetEveryFourthElement(List<double> elements)
         {
             List<double> everyFourth = new List<double>();
-
+            double[] empty = new double[0];
+            if (elements == null)
+            {
+                return empty;
+            }
             for (int i = 3; i < elements.Count; i += 4)
             {
                 everyFourth.Add(elements[i]);
             }
-
             return everyFourth.ToArray();
         }
-
         public bool TwoDifferentElementsInArrayCanSumToTargetNumber(int[] nums, int targetNumber)
         {
             for (int i = 0; i < nums.Length; i++)
             {
-                for (int k = i + 1; k < nums.Length; k++)
+                for (int j = i + 1; j < nums.Length; j++)
                 {
-                    if (nums[i] + nums[k] == targetNumber)
+                    if (nums[i] + nums[j] == targetNumber)
                     {
                         return true;
                     }
                 }
             }
-
             return false;
         }
     }
